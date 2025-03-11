6dac07fdf33f4e19ee001e3ae98e7b669c72f217
Joseph Andrade <joe.seph.andrade.ii@gmail.com>
2025-03-11 11:59:04 +0800

    add wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'signin',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'signin' // Replace '' with your default route 
   }
 ];
 
