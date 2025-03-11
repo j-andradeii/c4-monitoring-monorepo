02b6e9ff30e6b7f22aec047d324a811c06de5593
Joseph Andrade <joe.seph.andrade.ii@gmail.com>
2025-03-11 12:01:00 +0800

    add hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: CustomPreloadingStrategyService
+    preloadingStrategy: CustomPreloadingStrategyService,
+    useHash: true
 })],
   exports: [RouterModule]
 })
