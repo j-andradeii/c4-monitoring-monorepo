4421eef8714955900711e617dea42a145e641b0a
Joseph <joseph@jezweb.net>
2025-04-21 14:23:44 +0800

    update migration path

--- apps/church/src/orm-config.ts
+++ apps/church/src/orm-config.ts
@@ -4,11 +4,11 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 dotenvConfig({ path: '.env' });
 const entitiesPath = process.env.NODE_ENV === 'production'
-  ? __dirname + '/../../dist/apps/store/**/*.entity.js'  // Use JS files in production
+  ? __dirname + '/entities/*.entity.ts' // Use JS files in production
   : __dirname + '/entities/*.entity.ts';  // Use TS files in development
 
 const migrationsPath = process.env.NODE_ENV === 'production'
-  ? __dirname + '/../../dist/apps/store/db/migrations/*.js'  // Use JS files in production
+  ? __dirname + '/db/migrations/*.ts' // Use JS files in production
   : __dirname + '/db/migrations/*.ts';  // Use TS files in development
 
 // Log the paths for debugging
--- apps/members/src/orm-config.ts
+++ apps/members/src/orm-config.ts
@@ -4,23 +4,18 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 dotenvConfig({ path: '.env' });
 const entitiesPath = process.env.NODE_ENV === 'production'
-  ? __dirname + '/../../dist/apps/members/**/*.entity.js'  // Use JS files in production
+  ? __dirname + '/entities/*.entity.ts' // Use JS files in production
   : __dirname + '/entities/*.entity.ts';  // Use TS files in development
 
 const migrationsPath = process.env.NODE_ENV === 'production'
-  ? __dirname + '/../../dist/apps/members/db/migrations/*.js'  // Use JS files in production
+  ? __dirname + '/db/migrations/*.ts' // Use JS files in production
   : __dirname + '/db/migrations/*.ts';  // Use TS files in development
+
+console.log('__dirname: ', __dirname);
 
 // Log the paths for debugging
 console.log('Entities Path:', entitiesPath);
 console.log('Migrations Path:', migrationsPath);
-
-console.log("HOST-->", process.env.POSTGRES_HOST);
-console.log("PORT-->", process.env.POSTGRES_PORT);
-console.log("USERNAME-->", process.env.POSTGRES_USER);
-console.log("PASSWORD-->", process.env.POSTGRES_PASSWORD);
-console.log("DATABASE-->", process.env.POSTGRES_DB);
-console.log("DATABASEurl-->", process.env.DATABASE_URL);
 
 
 const config = {
