4019e95b6dc12fd4e338fe95a89345b92528fda0
Joseph <joseph@jezweb.net>
2025-04-22 01:05:28 +0800

    update typeorm cionfig

--- apps/members/src/orm-config.ts
+++ apps/members/src/orm-config.ts
@@ -35,8 +35,7 @@ const config = {
   autoLoadEntities: true,
   synchronize: false,
   extra: {
-    max: 20,
-    poolSize: parseInt(process.env.DB_POOL_SIZE || '15'), // Max connections per app instance (Increased default to 15)
+    max: parseInt(process.env.DB_POOL_MAX_CONNECTIONS || '15'), // Max connections
     connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT || '15000'), // How long to wait for a connection from the pool (Increased default to 15s)
     idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT || '30000'), // How long a connection can be idle before being closed
     query_timeout: parseInt(process.env.DB_QUERY_TIMEOUT || '10000'), // Max time (ms) per query (client-side)
